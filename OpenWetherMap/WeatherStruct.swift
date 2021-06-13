a06ea9369da650b9ae26762c37c79a7b29b3f340
Saliq <saliq.ilshat@icloud.com>
2021-06-13 11:48:21 +0500

    Create Weather structure

---
+++ OpenWetherMap/WeatherStruct.swift
@@ -0,0 +1,64 @@
+//
+//  WeatherStruct.swift
+//  OpenWetherMap
+//
+//  Created by Ильшат Мухамедьянов on 13.06.2021.
+//
+
+import Foundation
+
+struct WeatherStruct: Decodable {
+    var current: Current
+    var hourly: Hourly
+    var daily: Daily
+    var alerts: String
+}
+
+struct Current: Decodable {
+    var dt: Int
+    var sunrise: Int
+    var sunset: Int
+    var temp: Float
+    var feels_like: Float
+    var pressure: Int
+    var humidity: Int
+    var uvi: Float
+    var clouds: Int
+    var visibility: Int
+    var wind_speed: Float
+    var wind_deg: Int
+    var weather: [Weather]
+}
+
+struct Hourly:Decodable {
+    var dt: Int
+    var temp: Float
+    var weather: [Weather]
+}
+
+struct Daily: Decodable {
+    var dt: Int
+    var sunrise: Int
+    var sunset: Int
+    var temp: Temp
+    var pressure: Int
+    var humidity: Int
+    var weather: [Weather]
+    var clouds: Int
+    var pop: Int
+    var uvi: Float
+}
+
+struct Weather: Decodable {
+    var description: String
+    var icon: String
+}
+
+struct Temp: Decodable {
+    var day: Float
+    var min: Float
+    var max: Float
+    var night: Float
+    var eve: Float
+    var morn: Float
+}
